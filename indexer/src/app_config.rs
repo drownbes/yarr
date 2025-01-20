72022013ab785b33de1f3f15a753250be2f16bfb
Artem Markov <drownbes@gmail.com>
2025-01-20 17:00:26 +0200

    let go for prowlarr

---
+++ indexer/src/app_config.rs
@@ -0,0 +1,27 @@
+use std::path::Path;
+
+#[derive(serde::Deserialize, Clone, Debug)]
+pub struct AppConfig {
+    pub app: App,
+    pub db: Db
+}
+
+
+#[derive(serde::Deserialize, Clone, Debug)]
+pub struct App {
+    pub host: String,
+    pub port: u16
+}
+
+#[derive(serde::Deserialize, Clone, Debug)]
+pub struct Db {
+    pub conn_str: String,
+}
+
+pub fn read_config(cfg_path: &Path) -> Result<AppConfig, config::ConfigError> {
+    let cfg = config::Config::builder()
+        .add_source(config::File::from(cfg_path))
+        .build()?;
+
+    cfg.try_deserialize()
+}
---
+++ indexer/src/providers/mod.rs
@@ -0,0 +1,21 @@
+use std::future::Future;
+
+struct SearchResult;
+struct CategoriesResult;
+
+trait Provider: Send + Sync + 'static {
+    fn search(&self) -> impl Future<Output = anyhow::Result<SearchResult>>;
+    fn categories(&self) -> impl Future<Output = anyhow::Result<CategoriesResult>>;
+}
+
+struct RuTracker;
+
+impl Provider for RuTracker {
+    async fn search(&self) -> anyhow::Result<SearchResult> {
+        todo!()
+    }
+
+    async fn categories(&self) -> anyhow::Result<CategoriesResult> {
+        todo!()
+    }
+}
